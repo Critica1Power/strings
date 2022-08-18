5b6a397767811ecc8a09a3a982b37a4f6cd4bae5
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-18 21:48:13 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        // Задание 2
     }
 }
