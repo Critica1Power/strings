8542d0ddd95342645fdba3c0d493f11a9bfe6a0e
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-18 22:05:44 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich ";
-        String lastName = "Ivanov ";
+        String firstName = "ivan ";
+        String middleName = "ivanovich ";
+        String lastName = "ivanov ";
         String fullName = lastName + firstName + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
 
         // Задание 2
+        System.out.println("Задание 2");
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName1);
+
     }
 }
